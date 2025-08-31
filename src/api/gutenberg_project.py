fb4bc91984a2dda8ca77f0908d5ecd00dd6bad53
Kasra Madadipouya <kasra_mp@yahoo.com>
2025-08-31 20:13:21 +0200

    Integration with project Gutenberg

---
+++ src/api/gutenberg_project.py
@@ -0,0 +1,28 @@
+import requests
+from flask import render_template, Blueprint, request
+
+gutenberg_bp = Blueprint('gutenberg', __name__, url_prefix='/gutenberg')
+
+base_url = "https://gutendex.com/books/"
+
+
+@gutenberg_bp.route('/')
+def home():
+    return render_template('index_gutenberg.html')
+
+
+@gutenberg_bp.route('/search')
+def search():
+    # TODO - support multiple pages
+    query = request.args.get('q')
+    response = requests.get(base_url, params={"search": query})
+    books = response.json().get('results', [])
+    return render_template('result_gutenberg.html', query=query, results=books)
+
+
+@gutenberg_bp.route("/readability")
+def readability_page():
+    query = request.args.get('q')
+    book_id = request.args.get('id')
+    response = requests.get(f"{base_url}{book_id}").json()
+    return render_template('read_gutenberg.html', query=query, book=response)
--- src/app.py
+++ src/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from api.home import home_bp
 from api.gemini import gemini_bp
 from api.news import news_bp
+from api.gutenberg_project import gutenberg_bp
 
 app = Flask(__name__)
 app.register_blueprint(home_bp)
 app.register_blueprint(gemini_bp)
 app.register_blueprint(news_bp)
+app.register_blueprint(gutenberg_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
