66de799f1ee7e0a353ea781aea7a17352afecc0b
kasramp <kasra_mp@yahoo.com>
2025-09-04 21:31:48 +0000

    Faster build by running Arm image building on native Arm64 runner

--- util/release.sh
+++ util/release.sh
@@ -10,7 +10,10 @@ IMAGE_NAME=kasramp/kindler &&
 docker login -u "$DOCKER_USERNAME" --password-stdin <<< "$DOCKER_PASSWORD" &&
 docker buildx create --use &&
 docker buildx build --platform linux/amd64,linux/arm64 -t "$IMAGE_NAME":latest -t "$IMAGE_NAME:$TAGGED_VERSION" . --push
-git add version.txt &&
-git commit -m "Update version file to $TAGGED_VERSION" &&
-git push origin HEAD:master &&
+
+if [ "$ARCHITECTURE" = "linux/amd64" ]; then
+  git add version.txt &&
+  git commit -m "Update version file to $TAGGED_VERSION" &&
+  git push origin HEAD:master
+fi
 echo "Successfully build and pushed Docker $TAGGED_VERSION to Docker Hub"
